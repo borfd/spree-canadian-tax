f6e5f517737e7586ef2daaf48a5dbff0a1e485a2
Boris Filipov <bfilipov@gmail.com>
2015-02-16 14:00:26 +0100

    QST tax is applied after applying GST

--- lib/spree/calculator/quebec_tax.rb
+++ lib/spree/calculator/quebec_tax.rb
@@ -18,9 +18,10 @@ class Spree::Calculator::QuebecTax < Spree::Calculator
   private
 
   def calculate_taxation(total)
-    tps = total * 0.05
-    tvq = total * 0.09975 #TVQ
-    tax = round_to_two_places(tps + tvq)
+    gst = total * 0.05
+    qst = (total + gst) * 0.09975
+    tax = round_to_two_places(gst + qst)
+
     return tax
   end
 
